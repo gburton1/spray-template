bb6b039003dac0a367fd6e3f6d91474fe26ec257
Greg Burton <gburton@gmail.com>
2016-12-03 10:39:04 -0800

    new assembly target

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,8 @@ RUN chmod +x /run.sh
 
 COPY target/scala-2.11/source-assembly-0.1.jar /bs.jar
 
+HEALTHCHECK --interval=5s --timeout=3s CMD curl --fail http://localhost || exit 1
+
 CMD ["/run.sh"]
 
 EXPOSE 8080
--- run_tests.sh
+++ run_tests.sh
@@ -5,21 +5,13 @@ set -e
 if [ "$TEST_TYPE" = "integ" ]
 then
   sbt "testOnly *Integ"
-elif [ "$TEST_TYPE" = "watch-front" ]
+elif [ "$TEST_TYPE" = "assembly" ]
 then
-  mongod &
-  sbt run &
-  cd /source/client
-  Xvfb :1 -screen 0 1024x768x16 &>/dev/null  &
-  echo ">>> NPM is broken (again) so front-end tests are temporarily disabled."
-  # gulp watch
-  cat
+  cd /source
+  sbt assembly
 elif [ "$TEST_TYPE" = "all" ]
 then
   sbt "testOnly *Spec"
-  echo ">>> Running tests before generating JAR."
-  cd /source
-  sbt assembly
 else
   mongod &
   sbt "testOnly *Spec"
